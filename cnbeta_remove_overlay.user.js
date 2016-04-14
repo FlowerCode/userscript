6ec9a5462aa6cb29f430dc4238c09e5a57a2cffd
FlowerCode <FlowerCode@users.noreply.github.com>
2016-04-14 08:25:33 +0800

    Create cnbeta_remove_overlay.user.js

---
+++ cnbeta_remove_overlay.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         cnBeta.com Remove Overlay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove Overlay on cnBeta.com
+// @author       FlowerCode
+// @match        http://*.cnbeta.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var addedNodes = mutation.addedNodes;
+            for (var i = 0; i < addedNodes.length; i++) {
+                var node = addedNodes[i];
+                if ((node.nodeName == 'DIV') &&
+                    (node.style.display == 'block') &&
+                    (node.id))
+                {
+                    console.log('Removing node: ' + node.id);
+                    node.parentNode.removeChild(node);
+					observer.disconnect();
+                }
+            }
+
+        });
+    });
+
+    observer.observe(document, {childList: true, subtree: true});
+
+})();
